8692a91df03070bb2a36e2241e5d485dd9be75f9
XuXingLong <xxl20959@gmail.com>
2026-03-25 20:50:30 +0800

    Add files via upload
    calico token check and renew

---
+++ calico_token_renew.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+current_date=$(date +%s )
+
+if [ -f /etc/cni/net.d/calico-kubeconfig ]
+then
+ exp_date=$(cat /etc/cni/net.d/calico-kubeconfig | grep token | cut -d '.' -f2 | base64 -d  2>/dev/null | cut -d ',' -f2 | cut -d ':' -f2)
+else
+  echo "calico-kubeconfig文件不存在"
+  exit 1
+fi
+
+# 计算有效天数
+valid_day=$(echo "($exp_date-$current_date)/60/60/24" | bc)
+
+if [ $valid_day -gt 30 ]  # 30天告警级别（时间可自行修改）
+then
+  echo "--------------$(date +%F_%H:%M:%S)--------------"
+  echo "calico token check OK!"
+  echo "token有效剩余天数为：$valid_day天"
+  echo 
+elif [ $valid_day -gt 7 ]  # 7天危急级别（时间可自行修改）
+then
+  echo "--------------$(date +%F_%H:%M:%S)--------------"
+  echo "calico token check Warning!"
+  echo "token有效剩余天数为：$valid_day天，token即将过期！"
+  echo 
+else
+  echo "--------------$(date +%F_%H:%M:%S)--------------"
+  echo "calico token check Critical!"
+  echo "token有效剩余天数为：$valid_day天，calico pod将自动重启"
+  echo 
+
+  # 重启calico pod
+  calico_pods=$(kubectl get pod -n kube-system | grep calico-node | awk '{print $1}')
+  for calico_pod in $calico_pods
+  do
+    echo  "准备重启pod：$calico_pod"
+    kubectl delete pod -n kube-system $calico_pod
+  done
+fi
